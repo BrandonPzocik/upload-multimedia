7ca7e49ac4cfea84be833237bdcd13ab43d65fbf
Brandon <pzocikbrandon@gmail.com>
2024-10-16 14:37:34 -0300

    se conecto el front con el back y anda perfecto

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import { uploadImage } from "../middlewares/upload.middlewares.js";
+import { createProductCrtl } from "../controllers/products.controllers.js";
 
 const productsRouter = Router();
 
 //POST /products (create a new product)
-productsRouter.post("/", uploadImage("productImage"), (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    msg: "product created",
-  });
-});
+productsRouter.post("/", uploadImage("image"), createProductCrtl);
 
 export { productsRouter };
